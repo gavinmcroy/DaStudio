564bad3781089e089284fb57b7be0b3043327168
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2021-04-20 08:48:28 -0400

    buggy as hell

---
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
